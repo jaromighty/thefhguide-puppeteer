95a2ce109e3c21eeac767eed050118488ef54cbe
Jarom Rial <jarom.rial@gmail.com>
2023-01-19 14:47:29 -0700

    console names of dropdown links in navigation

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+  
+    await page.goto('https://thefhguide.com/');
+
+    const dropdownNames = await page.evaluate(() => {
+        const dropdowns = Array.from(document.querySelectorAll('.nav-item.dropdown'));
+        return dropdowns.map(dropdown => {
+            const name = dropdown.querySelector('.nav-link').innerText.trim();
+            return name;
+        });
+    }, );
+
+    console.log(dropdownNames);
+  
+    await browser.close();
+  })();
